e22bc2d3b8657deffc6e56c8279e1f17da35498b
ramzes600 <ramzes600@gmail.com>
2012-08-15 23:23:14 +0400

    Customers

---
+++ webapp/src/main/java/net/laba/app/customer/Customer.java
@@ -0,0 +1,85 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.laba.app.customer;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Rafael
+ */
+public class Customer {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private Date dateOfBirth;
+    private Date creationDate;
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+    
+    
+}
---
+++ webapp/src/main/java/net/laba/app/customer/CustomerHelper.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.laba.app.customer;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ *
+ * @author Rafael
+ */
+public class CustomerHelper {
+    private Integer ageCalcResult;
+    private Customer customer;
+    
+    
+    public void calculateAge(){
+        Date dateOfBirth = customer.getDateOfBirth();
+        Calendar birth = new GregorianCalendar();
+        birth.setTime(dateOfBirth);
+        Calendar now = new GregorianCalendar(2012,8,15);
+        ageCalcResult = now.get(Calendar.YEAR) - birth.get(Calendar.YEAR);
+        
+    }   
+    
+    //Not implemented yet
+    public Date getNextBirthDay(){
+     return null;
+    }
+    
+    public void clear(){
+    ageCalcResult = 0;
+    customer = null;
+    }
+
+    public Integer getAgeCalcResult() {
+        return ageCalcResult;
+    }
+
+   
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+    
+    
+    
+}
